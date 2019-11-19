82366c85505bce13c6113e853480800f60cf3827
Alessandro Rabitti <axel83.beta@gmail.com>
2019-11-19 14:00:33 +0100

    Improved plugin base script

--- DetachAllSymbols.sketchplugin/Contents/Sketch/script.js
+++ DetachAllSymbols.sketchplugin/Contents/Sketch/script.js
@@ -1,34 +1,33 @@
 const sketch = require('sketch')
-const pages = sketch.getSelectedDocument().pages
 const doc = sketch.getSelectedDocument()
+const pages = doc.pages
 
-//Search in document pages for symbols
-var inspectSelection = function(layers) {
-	layers.forEach(function(layers){
-		if(layers.layers != undefined){
-			inspectSelection(layers.layers)
+// Search in document pages for symbols
+const inspectSelection = layers => {
+	layers.forEach(layer => {
+		if (layer.layers) {
+			inspectSelection(layer.layers)
 		}
-		if(layers.layers == undefined){
-			if(layers.type == 'SymbolInstance'){
-				var group = layers.detach({
-					recursively: true,
+		else {
+			if(layer.type == 'SymbolInstance'){
+				layer.detach({
+					recursively: true
 				})
 			}
 		}
 	})
 }
 
-//Sends all document layers to detach
-var detachDocumentSymbols = function(){
-	pages.forEach(function(page) {
+// Send all document layers to detach
+const detachDocumentSymbols = () => {
+	pages.forEach(page => {
 		inspectSelection(page.layers)
 	})
-	sketch.UI.message(`🎉 All document symbols have been detached 🎉 `)
+	sketch.UI.message(`🎉 All document symbols have been detached 🎉`)
 }
 
-//Sends all selected layers to detach
-var detachSelectedSymbols = function(){
+// Sends all selected layers to detach
+const detachSelectedSymbols = () => {
 	inspectSelection(doc.selectedLayers.layers)
-	sketch.UI.message(`🎉 Selected symbols have been detached 🎉 `)
+	sketch.UI.message(`🎉 Selected symbols have been detached 🎉`)
 }
-
